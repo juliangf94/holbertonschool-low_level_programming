1c878bc01c54a271cfcca28f84a0de637a436014
juliangf94 <juliangf94@hotmail.com>
2025-10-21 15:56:07 +0200

    VA: Fix the 4-print_alphabt.c file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		} 
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
