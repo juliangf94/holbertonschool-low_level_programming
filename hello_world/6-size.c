c54b181297b258342c5ccdbbb05e3b6cf0b067c3
juliangf94 <juliangf94@hotmail.com>
2025-10-21 09:24:17 +0200

    HW: Add the 6-size.c file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Prints the size of various types on the computer it is compiled on.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	return (0);
+}
