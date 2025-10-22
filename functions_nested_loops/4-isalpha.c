23814427ec54777a1c857ac5c49c09f61079536a
juliangf94 <juliangf94@hotmail.com>
2025-10-22 15:49:10 +0200

    C-Functions: Add the 4-isalpha.c file

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isalpha - Checks for alphabeltic character.
+ * @c: The character (ASCII value) to be checked.
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	/*
+	 * Checks if the character is in the lowercase range
+	 *or in the uppercase range.
+	 */
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
