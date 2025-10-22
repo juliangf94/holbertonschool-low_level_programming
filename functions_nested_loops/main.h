899f8addd4f11847e52a5722d359bc8a4ab11c50
juliangf94 <juliangf94@hotmail.com>
2025-10-22 14:57:56 +0200

    C-Functions: Add the 1-alphabet.c file

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,4 +4,36 @@
 /*0-putchar.c -  Function prototype for _putchar */
 int _putchar(char c);
 
+/* 1-alphabet.c */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+/* 3-islower.c */
+int _islower(int c);
+
+/* 4-isalpha.c */
+int _isalpha(int c);
+
+/* 5-sSign.c */
+int print_sign(int n);
+
+/* 6-abs.c */
+int _abs(int);
+
+/* 7-print_last_digit.c */
+int print_last_digit(int);
+
+/* 8-24_hours.c */
+void jack_bauer(void);
+
+/* 9-times_table.c */
+void times_table(void);
+
+/* 10-add.c */
+int add(int, int);
+
+/* 11-print_to_98.c */
+void print_to_98(int n);
 #endif
