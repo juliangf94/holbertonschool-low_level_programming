283ef53c404a1ff998b5be24d0c8bee953e1382b
juliangf94 <juliangf94@hotmail.com>
2025-10-23 14:36:11 +0200

    C-Functions: Add the 8-24_hours.c file

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day from 00:00 to 23:59.
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h, m;
+	int h1, h2;
+	int m1, m2;
+
+	for (h = 0; h <=  23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			h1 = h / 10;
+			h2 = h % 10;
+			m1 = m / 10;
+			m2 = m % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
