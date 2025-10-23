49716c0f8105eebea67472e88af4666f400430c8
juliangf94 <juliangf94@hotmail.com>
2025-10-23 14:13:17 +0200

    C-Functions: Add the 6-abs.c file

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Calcula el valor absoluto de un entero.
+ *
+ * @n: El entero cuyo valor absoluto se va a calcular.
+ *
+ * Return: El valor absoluto del entero n.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (0);
+}
