52c30a2412e395d3223f0366f6e4fee95e78a5bd
juliangf94 <juliangf94@hotmail.com>
2025-10-27 14:49:22 +0100

    C-MF: Fix the 6-print_line.c file

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal.
+ *
+ * @n: The number of times the character '_' should be printed.
+ * If n is 0 or less, only a newline is printed.
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_')
+	}
+	_putchar('\n')
+}
