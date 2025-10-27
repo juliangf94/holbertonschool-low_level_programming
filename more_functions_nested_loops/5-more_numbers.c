ca7bfab78ed37a2605d3c94763c10598909bfc22
juliangf94 <juliangf94@hotmail.com>
2025-10-27 14:36:53 +0100

    C-MF: Add the 5-more_numbers.c file

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers from 0 to 14 ten times,
+ * followed by a new line after each sequence.
+ *
+ * Restriction: Can only use _putchar exactly three times.
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+	}
+	_putchar('\n');
+}
