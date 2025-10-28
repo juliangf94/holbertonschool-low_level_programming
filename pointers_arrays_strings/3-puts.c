bf9469024c4139e533359b780907cb61841dac2b
juliangf94 <juliangf94@hotmail.com>
2025-10-28 14:55:17 +0100

    C-PAS: Add the 3-puts.c file

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout, followed by a new line.
+ * @str: A pointer to the string to be printed.
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
