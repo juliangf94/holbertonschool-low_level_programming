b0f4fb74d9b40b3a2ac3a137d6903cc2f7fa1d49
juliangf94 <juliangf94@hotmail.com>
2025-10-28 09:21:35 +0100

    C-MF: Add the 10-print_triangle.c file

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a right-aligned triangle using the '#' character.
+ *
+ * @size: The size of the triangle (number of rows).
+ * If size is 0 or less, only a new line is printed.
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int space, row, symbol;
+
+	for (row = 0; row <= size; row++)
+	{
+		for (space = size - row; space > 0; space--)
+		{
+			_putchar(' ');
+		}
+		for (symbol = 0; symbol < row; symbol++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
