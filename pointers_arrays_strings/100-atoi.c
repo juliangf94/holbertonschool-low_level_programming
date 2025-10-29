d9db008dc0287cbf640715efeebb067eb2ae1554
juliangf94 <juliangf94@hotmail.com>
2025-10-29 15:03:49 +0100

    C-PAS: Add the 100-atoi.c file

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer, handling signs and preceding
+ * non-numeric characters.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer value. Returns 0 if no numbers are found.
+ */
+int _atoi(char *s)
+{
+	unsigned int result = 0;
+	int first_number = 0;
+	int sign = 1;
+
+	while (*s != '\0')
+	{
+		if (*s == ' ' && !first_number)
+		{
+			s++;
+			continue;
+		}
+
+		if (*s == '-' && !first_number)
+		{
+			sign *= -1;
+		}
+		else if (*s == '+' && !first_number)
+		{
+			continue;
+		}
+
+		if (*s >= '0' && *s <= '9')
+		{
+			first_number = 1;
+
+			result = (result * 10) + (*s - '0');
+		}
+		else if (first_number == 1)
+		{
+			break;
+		}
+		s++;
+	}
+	return (result * sign);
+}
