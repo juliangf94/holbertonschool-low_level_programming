1501ed09909de3098b5be2f0d56a5523a914c716
juliangf94 <juliangf94@hotmail.com>
2025-10-29 10:06:23 +0100

    C-PAS: Add the 6-puts2.c file

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first.
+ * @str: The pointer to the string.
+ *
+ * Description: Iterates through the string, printing characters at indices
+ * 0, 2, 4, 6, etc., until the null terminator is reached.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
