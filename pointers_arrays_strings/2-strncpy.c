48b7866e312ab9092093afe57c6278c497ecb44f
juliangf94 <juliangf94@hotmail.com>
2025-10-31 10:35:57 +0100

    C-MPAS: Add the -m 2-strncpy.c file

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string, similar to the standard strncpy.
+ * @dest: The destination buffer where the string will be copied.
+ * @src: The source string to copy.
+ * @n: The maximum number of bytes to copy.
+ *
+ * The function copies up to 'n' characters from the string pointed to by 'src'
+ * to the buffer pointed to by 'dest'.
+ *
+ * Standard strncpy behavior:
+ * 1. If 'src' is less than 'n' characters long, the remainder of 'dest'
+ * (up to the total of 'n' bytes) is padded with null bytes ('\0').
+ * 2. If 'src' is 'n' characters or longer, 'dest' will NOT be null-terminated
+ * (it is treated as a character array, not necessarily a string).
+ *
+ * Return: A pointer to the destination string 'dest'.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
