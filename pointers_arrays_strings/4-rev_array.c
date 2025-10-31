d2389658867090d8e43403cd98841335e799dcbf
juliangf94 <juliangf94@hotmail.com>
2025-10-31 13:49:36 +0100

    C-MPAS: Add the -m 4-rev-array.c file

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
+ *
+ * This function swaps elements from the beginning of the array with
+ * elements from the end until the middle of the array is reached,
+ * effectively reversing the array in place.
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
