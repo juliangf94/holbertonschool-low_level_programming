76c64616f7ef4b9edad466bb2fe9e1de5080bce6
juliangf94 <juliangf94@hotmail.com>
2025-11-05 22:18:43 +0100

    C-aa:Add the 2-args.c file

---
+++ argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints all command-line arguments received, one per line.
+ * @argc: The number of arguments
+ * (unused, but included for the main signature).
+ * @argv: An array of strings containing the arguments.
+ *
+ * Description: The function iterates through the argv array,
+ * starting at index 0,
+ * and prints each string followed by a newline character.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[i] != NULL)
+	{
+		printf("%d\n", argv[i]);
+		i++;
+	}
+	(void)argc;
+	return (0);
+}
