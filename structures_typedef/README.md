5dbacd73c8a0666781f24ce5348d225134cc4041
juliangf94 <juliangf94@hotmail.com>
2025-11-12 17:54:26 +0100

    CStructures: Add the dog.h file

---
+++ structures_typedef/README.md
@@ -0,0 +1,19 @@
+dog.h
+
+Header file containing the definition of struct dog and the typedef dog_t, along with function prototypes.
+
+1-init_dog.c
+
+Function to initialize a variable of type struct dog.
+
+2-print_dog.c
+
+Function to print the contents of a struct dog, handling NULL strings/pointers.
+
+4-new_dog.c
+
+Function to create a new dog_t dynamically, allocating memory for and copying the name and owner strings.
+
+5-free_dog.c
+
+Function to properly free the memory allocated by new_dog.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Stores information about a dog.
+ * @name: The name of the dog (char *).
+ * @age: The age of the dog (float).
+ * @owner: The owner of the dog (char *).
+ *
+ * Description: Defines a structure to hold details of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog.
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
