5373e325918e252e9cafcd1d38100658de497431
juliangf94 <juliangf94@hotmail.com>
2025-11-18 11:41:19 +0100

    cFPointers: Fix the 0-print_name.c file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -9,7 +9,10 @@ int _putchar(char c);
 /* 0. What's my name */
 void print_name(char *name, void (*f)(char *));
 
-/* 1. If you spend too much time thinking about a thing, you'll never get it done */
+/*
+ * 1. If you spend too much time thinking about a thing,
+ * you'll never get it done
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 /* 2. To hell with circumstances; I create opportunities */
